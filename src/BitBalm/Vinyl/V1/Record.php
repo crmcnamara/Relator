e628d9a4a8dd9fb12d7a5b937befa0a6318d1919
Bit Balm <hacks@bitbalm.null>
2019-07-18 22:36:59 -0700

    Vinyl\Record::initializeRecord change name and signature for initializeValues() to include explicit record id.

--- src/BitBalm/Vinyl/V1/Record.php
+++ src/BitBalm/Vinyl/V1/Record.php
@@ -6,7 +6,7 @@ namespace BitBalm\Vinyl\V1;
 
 interface Record
 {
-    public function initializeValues( array $values );
+    public function initializeRecord( $record_id, array $values );
     public function getRecordId();
     public function getUpdatedValues() : array ;
     public function getAllValues() : array ;
