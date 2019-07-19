17e9e42d5af5bcf6d35910aa0b7ee82b1e30d497
Bit Balm <hacks@bitbalm.null>
2019-07-18 22:41:28 -0700

    Vinyl\Record/Store: adding method docs.

--- src/BitBalm/Vinyl/V1/Record.php
+++ src/BitBalm/Vinyl/V1/Record.php
@@ -6,8 +6,33 @@ namespace BitBalm\Vinyl\V1;
 
 interface Record
 {
+    /**
+     * This method should be used to hydrate the record with values from the source database. 
+     * The record id and all key-value pairs valid for the Record should be passed.
+     * Implementors are free to require, prohibit, or ignore passing the record id 
+     *    as a field-value item in the $values argument.
+     */
     public function initializeRecord( $record_id, array $values );
+    
+    /**
+     * This method should provide the record id value passed to the last call to initializeValues(),
+     * even if the id value on the Record object itself is updated in the interim.
+     * This id value should be used to target the record in the source database to be updated
+     *    when the Record is to be saved.
+     */
     public function getRecordId();
+    
+    /**
+     * This method should be used to retrieve valid key-value pairs 
+     *    to be updated on the record in the source database
+     *    when the Record is to be saved.
+     * Implementations are not obligated to provide any values. 
+     */
     public function getUpdatedValues() : array ;
+    
+    /**
+     * This method should always provide all current field-value pairs that are valid for the record. 
+     * Implementations may or may not provide the record id in the returned array. 
+     */
     public function getAllValues() : array ;
 }
--- src/BitBalm/Vinyl/V1/RecordStore.php
+++ src/BitBalm/Vinyl/V1/RecordStore.php
@@ -3,15 +3,62 @@ declare (strict_types=1);
 
 namespace BitBalm\Vinyl\V1;
 
+use BitBalm\Vinyl\V1 as Vinyl;
+use BitBalm\Vinyl\V1\Exception\RecordNotFound;
+use BitBalm\Vinyl\V1\Exception\TooManyRecords;
+
 
 interface RecordStore
-{
+{   
+    /** 
+     * Returns a single record uniquely identified by its primary key id.
+     * Throws RecordNotFound when it comes up empty. 
+     */
     public function getRecord( $record_id ) : Record ;
+    
+    /**
+     * Returns a Collection of Records uniquely identified by the passed ids.
+     * The Collection may be empty if there are no matches. 
+     */
     public function getRecords( array $record_ids ) : Collection\Records ;
-    public function getRecordByFieldValues( string $field, $value ) : Record ; 
+    
+    /**
+     * Returns a single record 
+     *    uniquely identified by having the given value in the given field.
+     * Throws RecordNotFound when it comes up empty. 
+     * Throws TooManyRecords if more than one record matches.
+     * Throws InvalidArgumentException if the field is not valid for the record type.
+     */
+    public function getRecordByFieldValue( string $field, $value ) : Record ; 
+    
+    /**
+     * Returns a Collection of Records 
+     *    uniquely identified by having the given value in the given field.
+     * The Collection may be empty if there are no matches. 
+     * Throws InvalidArgumentException if the field is not valid for the record type.
+     */
     public function getRecordsByFieldValues( string $field, array $values ) : Collection\Records ;
     
+    
+    /**
+     * Creates a record in the source database with the given field-value pairs.
+     * Returns a new Record object initialized from the result.
+     */
     public function insertRecord( array $values ) : Record ;
+    
+    /**
+     * Updates the field-value pairs of the record in the source database
+     *    with the updated values on the Record object.
+     * Implementors should target the source database record using Record::getRecordId()
+     * Returns the same Record object after re-initializing it from the result.
+     * Throws RecordNotFound when it can't find the target record in the source database.
+     */
     public function updateRecord( Record $record ) : Record ;
+    
+    /**
+     * Deletes the record from the source database identified by the passed Record object.
+     * Implementors should target the source database record using Record::getRecordId()
+     * Throws RecordNotFound when it can't find the target record in the source database.
+     */
     public function deleteRecord( Record $record );
 }
