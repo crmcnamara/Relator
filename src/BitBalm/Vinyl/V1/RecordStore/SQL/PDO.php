45257036413adbe7ab0cd14c873accdc30deb358
Bit Balm <hacks@bitbalm.null>
2019-08-28 22:07:19 -0700

    Vinyl\RecordStore: removing some method requirements from SQL and PDO interfaces.

--- src/BitBalm/Vinyl/V1/RecordStore/SQL/PDO.php
+++ src/BitBalm/Vinyl/V1/RecordStore/SQL/PDO.php
@@ -6,20 +6,15 @@ namespace BitBalm\Vinyl\V1\RecordStore\SQL;
 use PDO as PDOConnection;
 use PDOStatement;
 
-use BitBalm\Vinyl\V1\RecordStore\SQL;
+use BitBalm\Vinyl\V1 as Vinyl;
 use BitBalm\Vinyl\V1\Record;
 use BitBalm\Vinyl\V1\Collection;
 
 
-interface PDO extends SQL
+interface PDO extends Vinyl\RecordStore\SQL
 {
     public function getPDO() : PDOConnection ;
     
-    public function getSelectStatement() : PDOStatement ;
-    public function getInsertStatement() : PDOStatement ;
-    public function getUpdateStatement() : PDOStatement ;
-    public function getDeleteStatement() : PDOStatement ;
-
     public function getRecordByStatement(  PDOStatement $statement, array $parameters ) : Record ;
     public function getRecordsByStatement( PDOStatement $statement, array $parameters ) : Collection\Records ;
     
