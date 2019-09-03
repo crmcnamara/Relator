38533f8e967f35e6b08304418adafc49f5163a91
Bit Balm <hacks@bitbalm.null>
2019-09-02 19:44:41 -0700

    Vinyl//Tests/RecordStore//Atlas.php: spit out more info on exceptions thrown from RecordStoreScenarios().

--- src/BitBalm/Vinyl/V1/Tests/RecordStore/SQL/PDO/Atlas.php
+++ src/BitBalm/Vinyl/V1/Tests/RecordStore/SQL/PDO/Atlas.php
@@ -21,28 +21,34 @@ class Atlas extends Vinyl\Tests\RecordStore\SQL\PDO
      */
     public function getRecordStoreScenarios() : array
     {
-        $scenarios = [];
-        
-        foreach ( $this->getSchemas() as $schema ) {
-            foreach ( $this->getPDOs() as $pdo ) {
-                
-                $schema->injectSchema($pdo);
-                $record_ids = $schema->injectRecords($pdo);
-                
-                foreach ( $record_ids as $table => $record_id ) {
-                    $scenarios[ implode( ' ', [ $table, get_class($schema), get_class($pdo), ] ) ] = [
-                        new Vinyl\RecordStore\SQL\PDO\Atlas( 
-                            $table, 
-                            new AtlasFactory( Connection::new($pdo) ),
-                            new Vinyl\Record\Generic,
-                            new Vinyl\Collection\Records
-                          ),
-                        $record_id,
-                      ];
+        try {
+            $scenarios = [];
+            
+            foreach ( $this->getSchemas() as $schema ) {
+                foreach ( $this->getPDOs() as $pdo ) {
+                    
+                    $schema->injectSchema($pdo);
+                    $record_ids = $schema->injectRecords($pdo);
+                    
+                    foreach ( $record_ids as $table => $record_id ) {
+                        $scenarios[ implode( ' ', [ $table, get_class($schema), get_class($pdo), ] ) ] = [
+                            new Vinyl\RecordStore\SQL\PDO\Atlas( 
+                                $table, 
+                                new AtlasFactory( Connection::new($pdo) ),
+                                new Vinyl\Record\Generic,
+                                new Vinyl\Collection\Records
+                              ),
+                            $record_id,
+                          ];
+                    }
                 }
             }
+            
+            return $scenarios;
+            
+        // PHPUnit isn't very forthcoming with error info in data providers
+        } catch ( \Throwable $x ) {
+            throw new \Exception((string)$x);
         }
-        
-        return $scenarios;
     }
 }
